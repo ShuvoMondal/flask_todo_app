6da48224fca69d205c2b6e8bc1adc153a210f99d
Tatsumi251 <mondalshuvo472@gmail.com>
2021-03-03 23:19:22 +0530

    Deleting cache files

--- __init__.py
+++ __init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from .main.routes import main
 from .extensions import mongo
 
+
 def create_app():
-    app=Flask(__name__)
-    app.config['MONGO_URI'] = 'mongodb+srv://admin:admin123@cluster0.x9lhp.mongodb.net/mydb?retryWrites=true&w=majority'
+    app = Flask(__name__)
+    app.config['MONGO_URI'] = ''
     app.register_blueprint(main)
     mongo.init_app(app)
 
